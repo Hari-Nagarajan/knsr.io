2e55da82cf667b3ca43b7adf14cba1a479a06074
hari nagarajan <hari@nagarajan.io>
2019-05-10 21:04:32 -0400

    content

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,18 @@
     <About />
     <Quote
       quote="KNSR gave me exponential growth in my
-               customer base. No matter what impediments we came across,
+               user base. No matter what impediments we came across,
                there was no stopping KNSR."
       name="Amar Patel"
-      location="Chicago, Il"
+      location="Chicago, IL"
     />
     <Team />
+    <Quote
+      quote="We were hesitant at first, but KNSR turned my business cash positive
+      and took us to the next level. I'm looking forward to the future."
+      name="Shauna Maddox"
+      location="New York, NY"
+    />
     <Footer />
   </div>
 </template>
