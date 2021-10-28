4bc984b310358ae39a0a4c92246290901d163ce2
hari <hari@nagarajan.io>
2021-10-27 23:16:11 -0400

    Dont sue me

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,6 +4,13 @@
       <p>
         Copyright ©2021 All Rights Reserved - KNSR LLC
       </p>
+      <p>
+      KNSR, LLC is a contracted Reseller of the First Data (and/or Clover) Solutions. This web page or website is not an official page of First Data Corporation, Clover, its subsidiaries, or affiliated businesses. © First Data Corporation. All rights reserved. The Clover name, logo and related trademarks are owned by Clover Network, Inc., a wholly owned subsidiary of First Data Corporation, and are registered or used in the U.S. and many foreign countries. All trademarks and registered trademarks are the property of their respective owners.
+      </p>
+
+      <p>
+      Some processing services provided by North American Bancard, LLC. All rights reserved. North American Bancard, LLC is a registered ISO of Wells Fargo Bank, N.A., Concord, CA., The Bancorp Bank, Philadelphia, PA, and BMO Harris N.A., Chicago, IL. 
+      </p>
     </div>
   </footer>
 </template>
